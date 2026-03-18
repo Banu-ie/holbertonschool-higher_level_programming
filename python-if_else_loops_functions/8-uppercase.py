bb8fe3ff55440da13c00d5dc1ea85cd5c8cf3c57
Banu-ie <i.banu.4757@gmail.com>
2026-03-18 10:12:52 +0000

    Create uppercase(str) function to print a string in uppercase without using str.upper()

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,5 @@
 def uppercase(str):
     """Print a string in uppercase followed by a new line."""
     for c in str:
-        # Print uppercase version if lowercase, else print character as-is
             print("{}".format(chr(ord(c) - 32) if 'a' <= c <= 'z' else c), end="")  
     print({}.format(""))  # 2nd print: just to add a newline
