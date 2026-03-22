71385cb5ebc1bdadc756f3bbbfb9f134465bc847
Banu-ie <i.banu.4757@gmail.com>
2026-03-22 18:26:31 +0000

    Implement max_integer to return the maximum value in a list or None if empty

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,9 +5,9 @@ def max_integer(my_list=[]):
         return None
 
     max_value = my_list[0]
-    
+
     for i in my_list:
         if i > max_value:
            max_value = i
-           
+
     return max_value
