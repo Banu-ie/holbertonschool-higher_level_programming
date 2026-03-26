4e2043b601c21416c4f8e54ce2ce1e70cd0e11d7
Banu-ie <i.banu.4757@gmail.com>
2026-03-26 10:58:55 +0000

    Implement multiply_by_2 to return a new dictionary with values doubled

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -5,3 +5,12 @@ def multiply_by_2(a_dictionary):
     for key in a_dictionary:
         new_dict[key] = a_dictionary[key] * 2
     return new_dict
+
+
+'''
+    def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return new_dict
+'''
