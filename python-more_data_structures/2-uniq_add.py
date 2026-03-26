1ba242438fb076bf23c1dd6e8bbb3eb0c5f11e6f
Banu-ie <i.banu.4757@gmail.com>
2026-03-26 09:22:49 +0000

    Implement uniq_add to sum unique integers in a list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_numbers = set(my_list)
+    return sum(unique_numbers)
+
+'''
+    def uniq_add(my_list=[]):
+        total = 0
+        new_list = []
+
+        for i in my_list:
+            if i is not in new_list:
+                total += i
+                new_list.append(i)
+            
+            return total
+'''
