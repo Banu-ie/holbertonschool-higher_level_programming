a2e076a22993a4735fbcda402adab9fb9eb700aa
Banu-ie <i.banu.4757@gmail.com>
2026-03-27 16:49:24 +0000

    0. Add function add_integer with doctests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,6 @@
 0. Integers addition
 """
 
-
 def add_integer(a, b=98):
     """
     Returns the addition of a and b.
@@ -11,10 +10,14 @@ def add_integer(a, b=98):
     a and b must be integers or floats; otherwise, raises TypeError.
     Floats are casted to integers before addition.
     """
+    # Type checks
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    
+    # Cast floats to int explicitly
+    a_int = int(a)
+    b_int = int(b)
+    
+    return a_int + b_int
